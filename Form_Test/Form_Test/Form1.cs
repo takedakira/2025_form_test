ebb413a9858f93df4ce5accd16f3e76c1afe8d29
kiraquin0623 <kiraquin0623@gmail.com>
2025-10-31 15:33:40 +0900

    kirigaiitokoro

--- Form_Test/Form_Test/Form1.cs
+++ Form_Test/Form_Test/Form1.cs
@@ -12,27 +12,31 @@ namespace Form_Test
 {
     public partial class Form1 : Form
     {
+        //constをつけると初期化時にのみ値の変更が可能になる
+        const int BUTTON_SIZE_X = 100;
+        const int BUTTON_SIZE_Y = 100;
+
+        const int BOARD_SIZE_X = 3;
+        const int BOARD_SIZE_Y = 3;
+
+
         public Form1()
         {
             InitializeComponent();
 
-            for (int i = 0; i < 6; i++)
+            for (int i = 0; i < 3; i++)
             {
-                for (int j = 0; j < 4; j++)
+                for (int j = 0; j < 3; j++)
                 {
-
-                    Test_Button test_Button = new Test_Button();
-
-                    test_Button.Location = new Point(50 * i, 50 * j);
-
-                    test_Button.Size = new Size(50, 50);
-
-                    test_Button.Text = "";
-
-                    test_Button.Click += button1_Click;
+                    //インスタンスの生成
+                    Test_Button test_Button 
+                        = new Test_Button(new Point(BUTTON_SIZE_X * i, BUTTON_SIZE_Y * j)
+                        , new Size(BUTTON_SIZE_X, BUTTON_SIZE_Y),"a");
 
 
 
+
+                  //コントロールにボタンを追加
                     Controls.Add(test_Button);
 
                 }
